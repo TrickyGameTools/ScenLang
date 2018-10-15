f1be59d6a8462da050e305f1b795e6b84a42cbd8
Tricky1975 <facebook@tbbs.nl>
2018-10-15 08:37:45 +0200

    MKL tags/info added

--- Data.cs
+++ Data.cs
@@ -1,10 +1,44 @@
+// Lic:
+// 	Scenario Language
+// 	Data
+// 	
+// 	
+// 	
+// 	(c) Jeroen P. Broks, 2015, 2017, 2018, All rights reserved
+// 	
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 		
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		
+// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
+// 	to the project the exceptions are needed for.
+// Version: 18.10.15
+// EndLic
 ﻿using System;
+using TrickyUnits;
 namespace ScenLang
 {
-    public class Data
+    static public class Data
     {
-        public Data()
+        static Data()
         {
+            MKL.Lic    ("Scenario Language - Data.cs","GNU General Public License 3");
+            MKL.Version("Scenario Language - Data.cs","18.10.15");
+        }
+
+
+        static public void LoadProject(string GINIFile)
+        {
+
+
         }
     }
 }
--- Main.cs
+++ Main.cs
@@ -1,3 +1,27 @@
+// Lic:
+// 	Scenario Language
+// 	Main
+// 	
+// 	
+// 	
+// 	(c) Jeroen P. Broks, 2015, 2017, 2018, All rights reserved
+// 	
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 		
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		
+// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
+// 	to the project the exceptions are needed for.
+// Version: 18.10.15
+// EndLic
 ﻿using System;
 using TrickyUnits;
 namespace ScenLang
@@ -6,8 +30,8 @@ namespace ScenLang
     {
         public static void Main(string[] args)
         {
-            MKL.Version("", "");
-            MKL.Lic("", "");
+            MKL.Version("Scenario Language - Main.cs","18.10.15");
+            MKL.Lic    ("Scenario Language - Main.cs","GNU General Public License 3");
             GUI.init(args);
             GUI.run();
         }
